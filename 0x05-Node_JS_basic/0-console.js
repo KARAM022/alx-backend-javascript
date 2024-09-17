15437e623f834849eb9e0962a90c93d21d111f06
KARAM022 <my0fav0library@gmail.com>
2024-09-17 18:55:53 +0100

    0x05

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+// module.exports = myFunction;
+
+// create a function named displayMessage that prints in STDOUT the string argument.
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
