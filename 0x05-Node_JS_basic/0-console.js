8772b7b6b289023e5a304841724c801c2f44188a
KARAM022 <my0fav0library@gmail.com>
2024-09-18 13:18:36 +0100

    0x05

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,11 @@
+/**
+ * CMNT: This is a simple function that takes a message and logs it to the console.
+ * @param {*} message
+ */
 function displayMessage(message) {
+    /**
+     * CMNT: This is a simple function that takes a message and logs it to the console.
+     */
     console.log(message);
 }
 
