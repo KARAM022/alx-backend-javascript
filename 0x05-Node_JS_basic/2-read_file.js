8df9d863916a6cce70a354124acacd4e85e147d1
KARAM022 <my0fav0library@gmail.com>
2024-09-19 21:25:37 +0100

    0x05

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,23 +5,23 @@ function countStudents(path) {
         const students = {};
         let count = 0;
 
-        for (const line of data) {
-            if (line) {
-                count++;
-                const student = line.split(',');
-                if (!fields[student[3]]) fields[student[3]] = [];
-                fields[student[3]].push(student[0]);
-                students[student[3]] = (students[student[3]] || 0) + 1;
-            }
-        }
+        // for (const line of data) {
+        //     if (line) {
+        //         count++;
+        //         const student = line.split(',');
+        //         if (!fields[student[3]]) fields[student[3]] = [];
+        //         fields[student[3]].push(student[0]);
+        //         students[student[3]] = (students[student[3]] || 0) + 1;
+        //     }
+        // }
 
-        console.log(`Number of students: ${count}`);
-        for (const field in fields) {
-            if (field) {
-                const list = fields[field];
-                console.log(`Number of students in ${field}: ${students[field]}. List: ${list.join(', ')}`);
-            }
-        }
+        // console.log(`Number of students: ${count}`);
+        // for (const field in fields) {
+        //     if (field) {
+        //         const list = fields[field];
+        //         console.log(`Number of students in ${field}: ${students[field]}. List: ${list.join(', ')}`);
+        //     }
+        // }
     }
     catch (err) {
         throw new Error('Cannot load the database');
